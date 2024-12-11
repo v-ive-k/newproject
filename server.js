6604e54bcb7b2e7ce31c15d11bd9fb4c7a60225b
v-ive-k <vivekkovi93@gmail.com>
2024-12-11 12:28:10 -0600

    Add files via upload
    added files via files direct os

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
